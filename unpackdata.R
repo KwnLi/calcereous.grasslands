757f52529a3395010dc8fab08dbebc4d9167765e
your-user-name-here <kevin.li@usda.gov>
2024-02-21 10:31:41 -0500

    remove ,

--- unpackdata.R
+++ unpackdata.R
@@ -18,6 +18,6 @@ for(i in 1:length(sel)){
   system(paste0("unzip", "'/90daydata/geoecoservices/calcereous/grass/*_", sel[i], "_*' -d ", grass.folder.i))
 
   imperv.folder.i <- paste0(datadir,"outdata/imperv/",sel[i])
-  dir.create(imperv.folder.i),
+  dir.create(imperv.folder.i)
   system(paste0("unzip", "'/90daydata/geoecoservices/calcereous/imperv/*_", sel[i], "_*' -d ", imperv.folder.i))
 }
