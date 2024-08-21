471505fb8b83216ef81aa412050fca523776b163
your-user-name-here <kevin.li@usda.gov>
2024-08-21 13:34:07 -0400

    out file names

--- batch_calc.sh
+++ batch_calc.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=atlas
 #SBATCH --qos=normal
 #SBATCH --job-name=calcgrass
-#SBATCH --output=/90daydata/geoecoservices/calcereous.grasslands/logs/missing-%j.out
-#SBATCH --error=/90daydata/geoecoservices/calcereous.grasslands/logs/missing-%j.err
+#SBATCH --output=/90daydata/geoecoservices/calcereous.grasslands/logs/calcgrass-%j.out
+#SBATCH --error=/90daydata/geoecoservices/calcereous.grasslands/logs/calcgrass-%j.err
 #SBATCH --account=geoecoservices
 #SBATCH --mail-user=kevin.li@usda.gov
 #SBATCH --mail-type=NONE
