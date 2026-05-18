0229d43865321de15937023679b61f9cf77f4831
Kevin <likevin@umich.edu>
2026-05-18 09:43:47 -0400

    batch script

--- scripts/batch_calc.sh
+++ scripts/batch_calc.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=calcgrass
 #SBATCH --account=hlc30_cr_default
 #SBATCH --partition=standard
-#SBATCH --output=/Users/kevinl/Documents/GitHub/calcereous.grasslands/logs/calcgrass-%A_%a.out
-#SBATCH --error=/Users/kevinl/Documents/GitHub/calcereous.grasslands/logs/calcgrass-%A_%a.err
+#SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/calcgrass-%A_%a.out
+#SBATCH --error=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/logs/calcgrass-%A_%a.err
 #SBATCH --time=24:00:00
 #SBATCH --mem=300G
 #SBATCH --nodes=1
@@ -14,4 +14,4 @@
 module load anaconda
 source activate r-geo
 
-Rscript --vanilla /Users/kevinl/Documents/GitHub/calcereous.grasslands/scripts/batch_grame.R ${SLURM_ARRAY_TASK_ID}
+Rscript --vanilla /storage/home/kbl5733/work/github/calcereous.grasslands/scripts/batch_grame.R ${SLURM_ARRAY_TASK_ID}
