17740e81e92116db8f0cc21b246673e38db9723b
Kevin <likevin@umich.edu>
2026-05-20 10:22:37 -0400

    up memory run

--- scripts/batch_extract.sh
+++ scripts/batch_extract.sh
@@ -9,7 +9,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --array=400              ### Array index
+#SBATCH --array=1-631              ### Array index
 
 module load anaconda
 source activate r-geo
