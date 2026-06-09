ce070287fadb05f500c2666176d742e161e5a4b6
Kevin Li <kbl5733@submit01.2e.hpc.psu.edu>
2026-06-09 10:55:54 -0400

    update batch script

--- scripts/batch_grassmask.sh
+++ scripts/batch_grassmask.sh
@@ -5,11 +5,11 @@
 #SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/maskgrass-%A.out
 #SBATCH --error=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/maskgrass-%A.err
 #SBATCH --time=01:00:00
-#SBATCH --mem=12G
+#SBATCH --mem=16G
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --array=1                ### Array index
+#SBATCH --array=3-631%10                ### Array index
 
 module load anaconda
 source activate r-geo
