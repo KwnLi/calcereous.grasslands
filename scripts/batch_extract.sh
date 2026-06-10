abbbe24a7fe0cf0dd0e163c2dadfe8272ef5ccc1
Kevin <likevin@umich.edu>
2026-06-09 22:33:09 -0400

    log file

--- scripts/batch_extract.sh
+++ scripts/batch_extract.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=extr_calc
 #SBATCH --account=open
 #SBATCH --partition=basic
-#SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/exp-%A_%a.out
-#SBATCH --error=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/exp-%A_%a.err
+#SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/extr_%A.out
+#SBATCH --error=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/extr_%A.err
 #SBATCH --time=1:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=1
