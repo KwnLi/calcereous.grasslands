6e670e08ce8290bc35fd013bb1b0f76a7f994925
Kevin Li <kbl5733@submit04.2e.hpc.psu.edu>
2026-06-11 09:41:25 -0400

    epsg config

--- scripts/check_tilelogs.R
+++ scripts/check_tilelogs.R
@@ -1,5 +1,7 @@
 #check tilelog
+library(tidyverse)
 tilelogfiles <- list.files("data-raw/big/tilelog", full.names = TRUE)
 tilelogfiles <- list.files(getwd(), pattern = ".csv$", full.names = TRUE)
+tilelogfiles <- list.files("/storage/group/hlc30/default/usda/Data/Calcereous/out/maskgrass/tilelog", pattern = ".csv$", full.names = TRUE)
 
 tilelogs <- lapply(tilelogfiles, read.csv) |> bind_rows()
