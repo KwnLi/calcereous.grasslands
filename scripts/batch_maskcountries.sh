d6fa5d164c8e6c233c187f4bf4776979222a133c
Kevin <likevin@umich.edu>
2026-08-01 17:25:18 -0400

    mask country code

---
+++ scripts/batch_maskcountries.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=maskcountry
+#SBATCH --account=open
+#SBATCH --partition=himem
+#SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/maskcountry-%A.out
+#SBATCH --error=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/maskcountry-%A.err
+#SBATCH --time=24:00:00
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1                ### Array index
+
+module load anaconda
+source activate r-geo
+
+Rscript --vanilla /storage/home/kbl5733/work/github/calcereous.grasslands/scripts/mask_countries.R ${SLURM_ARRAY_TASK_ID}
---
+++ scripts/mask_countries.R
@@ -0,0 +1,32 @@
+g <- as.numeric(commandArgs(trailingOnly = TRUE))
+suppressMessages(library(tidyverse))
+suppressMessages(library(terra))
+suppressMessages(library(sf))
+
+indir <- "/storage/home/kbl5733/gstorage/usda/Data/Calcereous/out/calcgrass/"
+outdir <- "/storage/home/kbl5733/gstorage/usda/Data/Calcereous/out/countries"
+
+grid.files <- list.files(indir, pattern = "*.tif$")
+
+grideu <- sf::st_read("data/eu_grid.gpkg")
+euclip <- sf::st_read("data/eu_clip.gpkg")
+
+country.g <- euclip$NAME_ENGL[g]
+
+print(paste("Clipping", country.g))
+
+country.g.buf <- sf::st_buffer(euclip[g,], 1000)   # buffer by 1km
+grid.g <- grideu[country.g.buf,]  # intersecting buffers
+tiles.g <-  gsub(".*(E\\d+N\\d+).*", "\\1", grid.g$CellCode)  # cell codes
+calcgrass.g <- paste0("calcgrass_",tiles.g,".tif")
+files.g <- grid.files[grid.files %in% calcgrass.g]   # grid files present in dir
+
+print(paste(length(files.g), "/", length(calcgrass.g), "files found for", country.g))
+
+rast.g <- lapply(file.path(indir, files.g), terra::rast)
+sprc.g <- sprc(rast.g)
+
+countrymerge <- terra::merge(sprc.g)
+
+fname <- paste0(country.g, "_calcgrass.tif")
+countrymask <- terra::mask(countrymerge, country.g.buf, filename = file.path(outdir, fname))
