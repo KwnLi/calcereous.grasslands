74b8f9b43d441cbad846d7177e3d7de629912af5
Kevin <likevin@umich.edu>
2026-08-03 07:03:53 -0400

    landscape metrics

---
+++ scripts/batch_landscapemetrics.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=maskcountry
+#SBATCH --account=open
+#SBATCH --partition=himem
+#SBATCH --output=/storage/home/kbl5733/work/github/calcereous.grasslands/logs/maskcountry-%A_%a.log
+#SBATCH --time=24:00:00
+#SBATCH --mem=20G
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --array=1-27%5                ### Array index
+
+module load anaconda
+source activate r-geo
+
+Rscript --vanilla /storage/home/kbl5733/work/github/calcereous.grasslands/scripts/landscapemetrics.R ${SLURM_ARRAY_TASK_ID}
--- scripts/landscapemetrics.R
+++ scripts/landscapemetrics.R
@@ -2,22 +2,30 @@ g <- as.numeric(commandArgs(trailingOnly = TRUE))
 suppressMessages(library(tidyverse))
 suppressMessages(library(terra))
 suppressMessages(library(sf))
+suppressMessages(library(landscapemetrics))
 
-indir <- "/storage/home/kbl5733/gstorage/usda/Data/Calcereous/out/countries"
-outdir <- "/storage/home/kbl5733/gstorage/usda/Data/Calcereous/out"
+datadir <- "/storage/home/kbl5733/gstorage/usda/Data/Calcereous/out"
+outdir <- file.path(datadir, "countrylm")
 
-country.files <- list.files(indir, pattern = "*.tif$")
+country.files <- list.files(file.path(datadir, "countries"), pattern = "*.tif$")
 
 country.g <- strsplit(country.files[g],"_")[[1]][1]
 
 print(paste("Landscape metrics for", country.g))
 
-country_calcgrass <-terra::rast(file.path(indir,country.files[g]))
+calcgrass.nobuf <-terra::rast(file.path(datadir,"countries",country.files[g]))
+calcgrass.buf <-terra::rast(file.path(datadir,"countries_buf",country.files[g]))
 
-rast.g <- lapply(file.path(indir, files.g), terra::rast)
-sprc.g <- sprc(rast.g)
+outdf <- list()
 
-countrymerge <- terra::merge(sprc.g)
+outdf[[areamn]] <- landscapemetrics::lsm_c_area_mn(calcgrass.nobuf)
+outdf[[areacv]] <- landscapemetrics::lsm_c_area_sd(calcgrass.nobuf)
+outdf[[areacv]] <- landscapemetrics::lsm_c_area_cv(calcgrass.nobuf)
+outdf[[areatot]] <- landscapemetrics::lsm_c_ca(calcgrass.nobuf)
+outdf[[ennmn]] <- landscapemetrics::lsm_c_enn_mn(calcgrass.buf)
+outdf[[enncv]] <- landscapemetrics::lsm_c_enn_sd(calcgrass.buf)
+outdf[[enncv]] <- landscapemetrics::lsm_c_enn_cv(calcgrass.buf)
 
-fname <- paste0(country.g, "_calcgrass.tif")
-countrymask <- terra::mask(countrymerge, country.g.buf, filename = file.path(outdir, fname))
+outdf <- dplyr::bind_rows(outdf)
+
+write.csv(outdf, file.path(outdir, paste0(country.g, "_lm.csv")), row.names = FALSE)
